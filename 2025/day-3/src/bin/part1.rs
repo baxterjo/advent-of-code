85d05a7c7268abc53ffe0e7b049fe0c766d64de8
baxterjo <j.bax080@gmail.com>
2025-12-02 23:32:26 -0800

    Day 3 took about an hour and a half

--- 2025/day-3/src/bin/part1.rs
+++ 2025/day-3/src/bin/part1.rs
@@ -1,10 +1,49 @@
 fn main() {
-    println!("Hello, world!");
+    let input = include_str!("./input1.txt");
+    process(input);
 }
 
-fn process(input: &str) {}
+fn process(input: &str) -> u64 {
+    let mut output = 0;
+    for line in input.lines() {
+        if line.is_empty() {
+            continue;
+        }
+
+        output += find_max(line);
+    }
+
+    println!("Output is {output}");
+    output
+}
+
+fn find_max(line: &str) -> u64 {
+    let mut out = 0;
+
+    let line_bytes = line.trim().as_bytes();
+
+    for i in 0..line_bytes.len() - 1 {
+        for j in i + 1..line_bytes.len() {
+            out = out.max(
+                format!("{}{}", line_bytes[i] as char, line_bytes[j] as char)
+                    .parse::<u64>()
+                    .expect("Invalid character."),
+            );
+        }
+    }
+    out
+}
 
 #[cfg(test)]
 mod test {
     use super::*;
+
+    #[test]
+    fn sample_works() {
+        let input = "987654321111111\n\
+                            811111111111119\n\
+                            234234234234278\n\
+                            818181911112111";
+        assert_eq!(process(input), 357)
+    }
 }
--- 2025/day-3/src/bin/part2.rs
+++ 2025/day-3/src/bin/part2.rs
@@ -1,3 +1,87 @@
+use tracing::{debug, info, instrument, trace};
+
 fn main() {
-    println!("Hello, world!");
+    let input = include_str!("./input1.txt");
+    process(input);
+}
+
+fn process(input: &str) -> u128 {
+    let mut output: u128 = 0;
+    for (idx, line) in input.lines().enumerate() {
+        if line.is_empty() {
+            continue;
+        }
+        let line_bytes = line.trim().as_bytes();
+        let mut out = "".to_string();
+        look_ahead_max(&mut out, line_bytes);
+
+        info!(out, len = out.len(), "Got out");
+        output = output
+            .checked_add(out.parse::<u128>().expect("Line returned bad output."))
+            .expect("Overflow!")
+    }
+
+    println!("Output is {output}");
+    output
+}
+
+fn look_ahead_max(out: &mut String, line_bytes: &[u8]) {
+    // If the out string is 12 digits long return.
+    if out.len() == 12 {
+        return;
+    }
+    debug!(
+        out,
+        line = String::from_utf8_lossy(line_bytes).to_string(),
+        line_len = line_bytes.len(),
+        "Checking remaining string"
+    );
+    let mut this_max = 0;
+    // Find the next max digit in the remaining line.
+    for i in 0..line_bytes.len() - (11 - out.len()) {
+        let this_num = (line_bytes[i] as char).to_digit(10).expect("Bad digit") as u64;
+        trace!(
+            this_max,
+            this_num,
+            this_num_bytes = line_bytes[i],
+            "Checking for new max"
+        );
+        this_max = this_max.max(this_num);
+    }
+    let idx = line_bytes
+        .iter()
+        .position(|&item| (item as char).to_digit(10).expect("Bad digit") as u64 == this_max)
+        .expect("Couldn't find digit I just found?");
+    // Update out
+    *out = format!("{out}{this_max}");
+    look_ahead_max(out, &line_bytes[idx + 1..line_bytes.len()]);
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn sample_works() {
+        let _ = tracing_subscriber::fmt::try_init();
+        let input = "987654321111111\n\
+                            811111111111119\n\
+                            234234234234278\n\
+                            818181911112111";
+        assert_eq!(process(input), 3121910778619)
+    }
+    #[test]
+    fn sub_sample_works() {
+        let _ = tracing_subscriber::fmt::try_init();
+        let line = "234234234234278";
+        let line_bytes = line.trim().as_bytes();
+        let mut out = "".to_string();
+        look_ahead_max(&mut out, line_bytes);
+
+        info!(out, len = out.len(), "Got out");
+        assert_eq!(
+            out.parse::<u128>().expect("Line returned bad output."),
+            434234234278
+        );
+    }
 }
